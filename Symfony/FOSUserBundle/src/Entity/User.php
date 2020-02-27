997ff3216178309876a2244222a480de6530ffb7
Yao Jean-Claude <jeandev84@yahoo.com>
2020-02-28 01:09:41 +0300

    FOSUserBundle using

---
+++ Symfony/FOSUserBundle/src/Entity/User.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+
+/**
+ * Class User
+ * @package App\Entity
+ *
+ * @ORM\Entity
+ * @ORM\Table(name="`user`")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+   */
+    protected $id;
+    // Note: The getter is not required because the parent class User
+    // already has the getId() method defined.
+}
